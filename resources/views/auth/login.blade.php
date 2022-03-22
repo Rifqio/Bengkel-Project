6bb73f84fc0aeff139afedf360140af52435090b
Rifqi Oktario <oktariorifqi@gmail.com>
2022-03-22 12:56:56 +0700

    Add login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.layout')
+
+@section('container')
+    <div class="min-h-screen bg-gray-200 font-openSans">
+        <div class="flex justify-center relative items-center justify-items-center pt-6">
+            <div class="bg-green-400 w-[450px] h-[600px] rounded-tl-xl rounded-bl-xl">
+                <img class="m-auto scale-75" src="/img/illustration1.svg" alt="">
+                <div class="-mt-6">
+                    <h1 class="text-center font-bold text-white text-2xl">Find The Right Parts <br> For Your Need</h1>
+                    <p class="text-center pt-4 text-sm text-white ">With BengkelAE you don’t need to worry <br> about
+                        replacing your vehicle spare parts!</p>
+                </div>
+            </div>
+            <div class="bg-white w-[600px] h-[600px] rounded-tr-xl rounded-br-xl">
+                <div class="pt-10">
+                    <div class="flex justify-center">
+                        <div class="bg-gray-100 w-24 h-24 rounded-full">
+                            <img class="m-auto pt-4 scale-90" src="/img/user.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="text-center pt-6">
+                        <h1 class="text-2xl font-bold">Create Your Free Account!</h1>
+                        <p class="text-sm font-light pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut egestas <br> nulla magna, in interdum purus tempus at.</p>
+                    </div>
+
+                    <div class="text-center pt-5">
+                        <form action="">
+                            <input class="border border-gray-200 w-[500px] h-10 rounded-md pl-4 mb-5 focus:outline-none focus:border-green-500 focus:ring-green-500 focus:ring-1" type="text" placeholder="Email">
+                            <input class="border border-gray-200 w-[500px] h-10 rounded-md pl-4 mb-5 focus:outline-none focus:border-green-500 focus:ring-green-500 focus:ring-1" type="password" placeholder="Password">
+                            {{-- <br> --}}
+                            {{-- <input class="bg-green-400 mb-5" type="checkbox" name="checkbox" id="">
+                            <label for="checkbox" class="text-sm opacity-50">By clicking continue, you agree to our terms and conditions</label> --}}
+                            <br>
+                            <button class="bg-green-400 w-[500px] h-[40px] rounded-md text-white font-semibold hover:bg-green-700" type="submit">Continue</button>
+                        </form>
+                        <p class="pt-4">Already have an account? <a href="" class="text-green-400 hover:text-green-700">Log in</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
