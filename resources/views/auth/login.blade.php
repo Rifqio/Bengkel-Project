4c5a3584e4d9efc79404e4672d2d401dbd5cf054
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-03-28 17:05:56 +0700

    Added jetstream register and login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,48 @@
-@extends('layout.layout')
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
 
-@section('container')
-<div class="min-h-screen bg-gray-200 font-openSans">
-    <div class="flex justify-center relative items-center justify-items-center pt-6 2xl:pt-20">
-        <div class="bg-gradient-to-t from-[#47BF71] to-[#67DA8F] 2xl:w-[600px] 2xl:h-[800px] w-[450px] h-[600px] rounded-tl-xl rounded-bl-xl shadow-lg">
-            <img class="mt-12 2xl:mt-24 scale-75" src="/img/login.svg" alt="">
-            <div class="text-center text-white">
-                <h1 class="2xl:text-2xl md:text-xl font-bold capitalize">Your one stop repair solution</h1>
-                <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> Placeat, eveniet!</p>
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
 
-        <div class="bg-white 2xl:w-[1000px] 2xl:h-[800px] xl:w-[600px] xl:h-[600px] rounded-tr-xl rounded-br-xl shadow-lg">
-            <div class="xl:pt-10 2xl:pt-32">
-                <div class="flex justify-center">
-                    <div class="bg-gray-100 w-24 h-24 rounded-full hover:bg-gray-300">
-                       <a href="/"><img class="m-auto pt-4 scale-90 2xl:pt-5 " src="/img/user_check.svg" alt=""></a>
-                    </div>
-                </div>
-                <div class="text-center pt-6">
-                    <h1 class="text-2xl font-bold 2xl:text-3xl">Welcome Back!</h1>
-                    <p class="text-sm 2xl:text-base font-light pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut egestas <br> nulla magna, in interdum purus tempus at.</p>
-                </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-                <div class="text-center pt-5">
-                    <form action="">
-                        <input class="border border-gray-200 w-[500px] h-10 rounded-md pl-4 mb-5 focus:outline-none focus:border-green-500 focus:ring-green-500 focus:ring-1" type="text" placeholder="Email">
-                        <input class="border border-gray-200 w-[500px] h-10 rounded-md pl-4 mb-5 focus:outline-none focus:border-green-500 focus:ring-green-500 focus:ring-1" type="password" placeholder="Password">
-                        <br>
-                        <button class="bg-green-10 w-[500px] h-[40px] rounded-md text-white font-semibold hover:bg-green-700" type="submit">Login</button>
-                    </form>
-                    <p class="pt-4">Don't have an account yet? <a href="/register" class="text-green-10 hover:text-green-700">Register</a></p>
-                </div>
+            <div>
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
-        </div>
-    </div>
 
-</div>
-@endsection
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-jet-button class="ml-4">
+                    {{ __('Log in') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-        colors: {
-            green: {
-                10: '#47BF71'
-            }
-        }
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    fontFamily: {
-        openSans: ['Open Sans', 'sans-serif']
-    },
-  },
-  plugins: [],
-}
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
