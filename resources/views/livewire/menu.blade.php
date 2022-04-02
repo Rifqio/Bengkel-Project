265487dff50b04429ed77c8b6a7f8909b6301de1
Rifqi Oktario <oktariorifqi@gmail.com>
2022-04-02 15:27:04 +0700

    Add user dashboard

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -19,10 +19,20 @@
 
     <div class="bg-white w-[200px] h-[200px] border border-gray-400 rounded-md drop-shadow-md mx-auto">
         <div class="py-4">
+            <img class="w-[90px] h-[90px] mx-auto" src="/img/pointer.svg" alt="">
+            <div class="pt-4">
+                <h1 class="font-bold">Repair</h1>
+                <p class="text-sm max-w-[120px]  mx-auto text-gray-500">Find nearest repair shop in your area</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white w-[200px] h-[200px] border border-gray-400 rounded-md drop-shadow-md mx-auto">
+        <div class="py-4">
             <img class="w-[90px] h-[90px] mx-auto" src="/img/toolbox.svg" alt="">
             <div class="pt-4">
                 <h1 class="font-bold">Tools</h1>
-                <p class="text-sm text-gray-500">Tools for maintaining your car</p>
+                <p class="text-sm max-w-[150px]  mx-auto text-gray-500">Tools for maintaining your car</p>
             </div>
         </div>
     </div>
