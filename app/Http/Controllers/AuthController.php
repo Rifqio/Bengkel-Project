90ee53fecf167301a4908cb173dd8de4f6e80130
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-04-12 11:42:22 +0700

    Create User By Admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 
@@ -18,30 +19,40 @@ class AuthController extends Controller
 
     public function CreateEmployee(Request $request)
     {
-        Validator::make($request->except(['_token']), [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'nik' => ['required', 'string', 'max:16', 'min:16', 'unique:users,nik'],
-            'role' => ['required'],
-            'password' => 'min:8|required_with:password_confirmation|same:password_confirmation',
-            'password_confirmation' => 'min:8',
-        ])->validate();
+        DB::beginTransaction();
+        try{
+            Validator::make($request->except(['_token']), [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'nik' => ['required', 'string', 'max:16', 'min:16', 'unique:users,nik'],
+                'role' => ['required'],
+                'password' => 'min:8|required_with:password_confirmation|same:password_confirmation',
+                'password_confirmation' => 'min:8',
+            ])->validate();
 
-        $user = User::create([
-            'name' => request()->name,
-            'email' => request()->email,
-            'nik' => request()->nik,
-            'email_verified_at' => Carbon::now()->toDateTimeString(),
-            'password'=> Hash::make(request()->password),
-        ]);
-        $user->attachRole($request->role);
+            $user = User::create([
+                'name' => request()->name,
+                'email' => request()->email,
+                'nik' => request()->nik,
+                'email_verified_at' => Carbon::now()->toDateTimeString(),
+                'password'=> Hash::make(request()->password),
+            ]);
+            $user->attachRole($request->role);
 
-        $data = array('name'=>request()->name, 'email'=>request()->email, 'password'=> request()->password);
-        Mail::send('email.em-sa-email', $data, function($message) {
-            $message->to(request()->email, 'Test Email')->subject
-               ('Email dan Password Employee');
-            $message->from(Auth::user()->email, Auth::user()->name);
-        });
-        return redirect('/dashboard');
+            $data = array('name'=>request()->name, 'email'=>request()->email, 'password'=> request()->password);
+            Mail::send('email.em-sa-email', $data, function($message) {
+                $message->to(request()->email, 'Test Email')->subject
+                ('Email dan Password Employee');
+                $message->from(Auth::user()->email, Auth::user()->name);
+            });
+            
+            DB::commit();
+            
+            return redirect('/dashboard');
+        }catch(\Exception $e){
+            DB::rollback();
+            echo 'Error Exception';
+        }
+
     }
 }
