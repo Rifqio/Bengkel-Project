9cf83c174911a9d2d2cd097b3138214a0eea6140
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2022-04-23 17:47:42 +0700

    up

--- resources/views/user/usersparepart.blade.php
+++ resources/views/user/usersparepart.blade.php
@@ -11,24 +11,25 @@
 
 <body>
 
-    <header class="bg-white fixed top-0 left-0 w-full items-end z-10 py-5 opacity-75">
-        <div class="flex w-32 border justify-end bg-slate-600 pr-10 ">
-            <div>
-                <form action="">
-                    <input type="text" placeholder="Search for a part..." class="px-2 py-1 border shadow-sm rounded">
-                </form>
+    <header class="bg-white fixed top-0 left-0 w-full items-end z-10 py-3 opacity-75">
+        <div class="flex w-32 justify-end bg-slate-600 pr-10 ">
+            <div class="flex border-2 justify-end shadow-sm">
+                <div>
+                    <form action="">
+                        <input type="text" placeholder="Search for a part..." class="px-2 py-1  border-none">
+                    </form>
+                </div>
+                <div class=""><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <circle cx="10" cy="10" r="7"></circle>
+                        <line x1="21" y1="21" x2="15" y2="15"></line>
+                    </svg></div>
             </div>
-            <div class=""><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="10" cy="10" r="7"></circle>
-                    <line x1="21" y1="21" x2="15" y2="15"></line>
-                </svg></div>
         </div>
-
     </header>
 
-    <Section id="banner" class="bg-gradient-to-r bg-gradient-to-r from-ijo to-white ">
-        <div class="px-10 pt-5">
+    <Section id="banner" class="bg-gradient-to-r from-green-700 to-ijo ">
+        <div class="px-10 pt-8">
             <div class="container mx-auto">
                 <h1 class="text-4xl  py-10 text-left font-bold text-white uppercase">Compare Part</h1>
                 <p class="w-3/4 md:w-1/2 lg:1/4 text-white font-bold pb-28 ">The comparison function is to find out whether there are differences in variables between one group and another. Comparison is an observation that aims to find ut and/or test the differences between two or more groups.</p>
