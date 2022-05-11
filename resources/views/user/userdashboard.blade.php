5c9fb80e4f2c645e22683d2ff243a8746d078a17
Syi'ta Al Mar'atush Sholihah <syitatahasa@student.uns.ac.id>
2022-05-11 21:32:45 +0700

    coba2

--- resources/views/user/userdashboard.blade.php
+++ resources/views/user/userdashboard.blade.php
@@ -7,7 +7,7 @@
                 </svg>
 
                 <div>
-                    <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+                    <div class="relative pt-6 px-4 sm:px-6 lg:px-8">z
                         <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
                             <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                                 <div class="flex items-center justify-between w-full md:w-auto">
@@ -82,7 +82,7 @@
                 <main class="mt-10 mx-auto max-w-7xl 2xl:max-w-max px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                     <div class="sm:text-center lg:text-left">
                         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                            <span class="text-green-500 xl:inline">BengkelAE</span>
+                            <span class="text-green-500 xl:inline">BengkelAE khaksuhkjdkljlkj coba</span>
 
                         </h1>
                         <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
