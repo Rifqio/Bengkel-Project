d9c7fc6b68b9dd4e4068c27895866677868325d6
Syi'ta Al Mar'atush Sholihah <syitatahasa@student.uns.ac.id>
2022-05-14 22:01:09 +0700

    coba beberapa kali

--- resources/views/user/userdashboard.blade.php
+++ resources/views/user/userdashboard.blade.php
@@ -114,8 +114,7 @@
     <div class="py-12">
         <div class="max-w-5xl px-2 md:px-0 mx-auto text-center md:max-w-7xl">
             <h1 class="text-xl font-semibold capitalize pb-4">Hi how can we help you today?</h1>
-            {{-- Menu dalam dashboard --}}
-            @livewire('menu')
+            
         </div>
         <center>
             <center>
