e5a326c484afde0e9aefeeae0103ebc041cef5ce
unknown <shallomsitorus31@gmail.com>
2022-05-16 13:36:43 +0700

    menambahkan public function update dan deletedi empcontroller

--- app/Http/Controllers/EmpController.php
+++ app/Http/Controllers/EmpController.php
@@ -28,12 +28,54 @@ class EmpController extends Controller
         ]);
     }
 
-    public function UpdateDataMitra($id, Request $request){
-        //dd($request->except(['_token']));
-        //Untuk Shallom
-        //Tambahkan Validasi Input
-        //Update Data dan Delete Data
-        $user = User::find($id);
-        dd($user);
-    }
+    // public function EditDataMitra($id, Request $request){
+    //     //dd($request->except(['_token']));
+    //     //Untuk Shallom
+    //     //Tambahkan Validasi Input
+    //     //Update Data dan Delete Data
+    //     $user = User::find($id);
+    //     // dd($user);
+
+    //     // return view('nama folder.nama file', compact(
+    //     //     'user'
+    //     // ));
+    //     return view('admin.listmitra', compact(
+    //         'user'
+    //     ));
+    // }
+    public function UpdateDataMitra(Request $request){
+        $model = User::find(request()->id);
+        $model->update($request->except(['id']));
+        return redirect('list-mitra')->with('success', 'User has been updated');
+        // $model = User::find($id);
+        // dd($model);
+        // $model->update($request->except(['_token','id']));
+        // $model->update();
+        // return view('admin.listmitra', compact(
+        //     'user'
+        // ));
 }
+
+public function destroy($id)
+{
+    $users = User::find($id);
+        $users->delete();
+        return redirect('list-mitra');
+}
+}
+//     public function DeleteDataMitra($id){
+//         $users = User::findOrFail($id);
+//         $users->delete();
+//         return redirect('list-mitra')->with('success', 'User has been deleted');
+//     //     $model = User::find(request()->id);
+//     //     $model->update($request->except(['id']));
+//     //     return redirect('list-mitra');
+//         // $model = User::find($id);
+//         // dd($model);
+//         // $model->update($request->except(['_token','id']));
+//         // $model->update();
+//         // return view('admin.listmitra', compact(
+//         //     'user'
+//         // ));
+// }
+// }
