d0d74342c6cf6313f48ae75f8d90c190de3d569a
unknown <shallomsitorus31@gmail.com>
2022-05-16 13:41:41 +0700

    menghapus

--- app/Http/Controllers/EmpController.php
+++ app/Http/Controllers/EmpController.php
@@ -28,32 +28,10 @@ class EmpController extends Controller
         ]);
     }
 
-    // public function EditDataMitra($id, Request $request){
-    //     //dd($request->except(['_token']));
-    //     //Untuk Shallom
-    //     //Tambahkan Validasi Input
-    //     //Update Data dan Delete Data
-    //     $user = User::find($id);
-    //     // dd($user);
-
-    //     // return view('nama folder.nama file', compact(
-    //     //     'user'
-    //     // ));
-    //     return view('admin.listmitra', compact(
-    //         'user'
-    //     ));
-    // }
     public function UpdateDataMitra(Request $request){
         $model = User::find(request()->id);
         $model->update($request->except(['id']));
         return redirect('list-mitra')->with('success', 'User has been updated');
-        // $model = User::find($id);
-        // dd($model);
-        // $model->update($request->except(['_token','id']));
-        // $model->update();
-        // return view('admin.listmitra', compact(
-        //     'user'
-        // ));
 }
 
 public function destroy($id)
@@ -63,19 +41,3 @@ public function destroy($id)
         return redirect('list-mitra');
 }
 }
-//     public function DeleteDataMitra($id){
-//         $users = User::findOrFail($id);
-//         $users->delete();
-//         return redirect('list-mitra')->with('success', 'User has been deleted');
-//     //     $model = User::find(request()->id);
-//     //     $model->update($request->except(['id']));
-//     //     return redirect('list-mitra');
-//         // $model = User::find($id);
-//         // dd($model);
-//         // $model->update($request->except(['_token','id']));
-//         // $model->update();
-//         // return view('admin.listmitra', compact(
-//         //     'user'
-//         // ));
-// }
-// }
