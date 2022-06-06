91f1a1b8e6ec0cab343647e35aff5e085e78aa1b
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-06-06 12:56:34 +0700

    Checking

--- resources/views/SuperAdmin/list-bengkel-aktif.blade.php
+++ resources/views/SuperAdmin/list-bengkel-aktif.blade.php
@@ -302,100 +302,122 @@
                                 <th>Action</th>
                             </tr>
                             @foreach($stores as $s)
-                            <tr>
-                                <td>
-                                    {{$loop->iteration}}
-                                </td>
-                                <td>
-                                    {{$s->store_name}}
-                                </td>
-                                <td>
-                                    {{$s->users->name}}
-                                </td>
-                                <td>
-                                    {{$s->users->email}}
-                                </td>
-                                <td>
-                                    {{$s->phone_store}}
-                                </td>
-                                <td class="align-middle text-sm">
-                                    {{$s->address}}
-                                </td>
-                                <td class="align-middle text-sm">
-                                    <button type="button" class="btn btn-block bg-gradient-danger mb-3" data-bs-toggle="modal" data-bs-target="#deactive{{$s->id}}">
-                                        Nonaktif
-                                    </button>
-                                    <button type="button" class="btn btn-block bg-gradient-info mb-3" data-bs-toggle="modal" data-bs-target="#detail{{$s->id}}">
-                                        Detail
-                                    </button>
-                                </td>
-                            </tr>
-                            {{-- Deactive --}}
-                            <div class="modal fade" id="deactive{{$s->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-                                <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h6 class="modal-title" id="modal-title-default">Non Aktivasi Bengkel</h6>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">×</span>
-                                      </button>
+                                <tr>
+                                    <td>
+                                        {{ $loop->iteration }}
+                                    </td>
+                                    <td>
+                                        {{ $s->store_name }}
+                                    </td>
+                                    <td>
+                                        {{ $s->users->name }}
+                                    </td>
+                                    <td>
+                                        {{ $s->users->email }}
+                                    </td>
+                                    <td>
+                                        {{ $s->phone_store }}
+                                    </td>
+                                    <td class="align-middle text-sm">
+                                        {{ $s->address }}
+                                    </td>
+                                    <td class="align-middle text-sm">
+                                        <button type="button" class="btn btn-block bg-gradient-danger mb-3"
+                                            data-bs-toggle="modal" data-bs-target="#deactive{{ $s->id }}">
+                                            Nonaktif
+                                        </button>
+                                        <button type="button" class="btn btn-block bg-gradient-info mb-3"
+                                            data-bs-toggle="modal" data-bs-target="#detail{{ $s->id }}">
+                                            Detail
+                                        </button>
+                                    </td>
+                                </tr>
+                                {{-- Deactive --}}
+                                <div class="modal fade" id="deactive{{ $s->id }}" tabindex="-1" role="dialog"
+                                    aria-labelledby="modal-default" aria-hidden="true">
+                                    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h6 class="modal-title" id="modal-title-default">Non Aktivasi Bengkel
+                                                </h6>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">×</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Apakah anda yakin untuk nonaktifkan Bengkel "{{ $s->store_name }}"
+                                                </p>
+                                                <p>Pemilik : {{ $s->users->name }}</p>
+                                                <p>Email : {{ $s->users->email }}</p>
+                                                <p>NIK : {{ $s->users->nik }}</p>
+                                                <br>
+                                                <p>Bengkel Dapat Diaktifkan Melalui Tab Validasi Bengkel atau SuperAdmin
+                                                </p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <form action="{{ url('non-aktif') }}" method="POST">
+                                                    @csrf
+                                                    <input type="hidden" name="id" value="{{ $s->id }}">
+                                                    <input type="hidden" name="status" value="0">
+                                                    <button type="submit" class="btn bg-gradient-danger">Save
+                                                        changes</button>
+                                                    <button type="button" class="btn btn-link  ml-auto"
+                                                        data-bs-dismiss="modal">Close</button>
+                                                </form>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="modal-body">
-                                      <p>Apakah anda yakin untuk nonaktifkan Bengkel "{{$s->store_name}}"</p>
-                                      <p>Pemilik : {{$s->users->name}}</p>
-                                      <p>Email : {{$s->users->email}}</p>
-                                      <p>NIK : {{$s->users->nik}}</p>
-                                      <br>
-                                      <p>Bengkel Dapat Diaktifkan Melalui Tab Validasi Bengkel atau SuperAdmin</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                    <form action="{{url('non-aktif')}}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="id" value="{{$s->id}}">
-                                    <input type="hidden" name="status" value="0">
-                                      <button type="submit" class="btn bg-gradient-danger">Save changes</button>
-                                      <button type="button" class="btn btn-link  ml-auto" data-bs-dismiss="modal">Close</button>
-                                    </form>
-                                    </div>
-                                  </div>
                                 </div>
-                            </div>
-                                                            <!-- Modal Detail -->
-                                                            <div class="modal fade" id="detail{{$s->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalMessageTitle" aria-hidden="true">
-                                                                <div class="modal-dialog modal-dialog-centered" role="document">
-                                                                <div class="modal-content">
-                                                                    <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">New message to @CT</h5>
-                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                                        <span aria-hidden="true">×</span>
-                                                                    </button>
-                                                                    </div>
-                                                                    <div class="modal-body">
-                                                                    <form>
-                                                                        <div class="form-group">
-                                                                            <label for="recipient-name" class="col-form-label">Nama Bengkel:</label>
-                                                                            <input type="text" class="form-control" value="{{$s->store_name}}" id="recipient-name" readonly>
-                                                                        </div>
-                                                                        <div class="form-group">
-                                                                            <label for="message-text" class="col-form-label">Pemilik:</label>
-                                                                            <input type="text" class="form-control" value="{{$s->users->name}}" id="recipient-name" readonly>
-                                                                        </div>
-                                                                        <div class="form-group">
-                                                                            <label for="message-text" class="col-form-label">Telefon Bengkel:</label>
-                                                                            <input type="text" class="form-control" value="{{$s->phone_store}}" id="recipient-name" readonly>
-                                                                        </div>
-                                                                        <div class="form-group">
-                                                                            <label for="message-text" class="col-form-label">Tanggal Diajukan:</label>
-                                                                            <input type="text" class="form-control" value="{{$s->updated_at}}" id="recipient-name" readonly>
-                                                                        </div>
-                                                                    </form>
-                                                                    </div>
-                                                                    <div class="modal-footer">
-                                                                    <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
-                                                                    </div>
-                                                                </div>
-                                                                </div>
-                                                            </div>
+                                <!-- Modal Detail -->
+                                <div class="modal fade" id="detail{{ $s->id }}" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalMessageTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">New message to @CT</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">×</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form>
+                                                    <div class="form-group">
+                                                        <label for="recipient-name" class="col-form-label">Nama
+                                                            Bengkel:</label>
+                                                        <input type="text" class="form-control"
+                                                            value="{{ $s->store_name }}" id="recipient-name" readonly>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="message-text"
+                                                            class="col-form-label">Pemilik:</label>
+                                                        <input type="text" class="form-control"
+                                                            value="{{ $s->users->name }}" id="recipient-name"
+                                                            readonly>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="message-text" class="col-form-label">Telefon
+                                                            Bengkel:</label>
+                                                        <input type="text" class="form-control"
+                                                            value="{{ $s->phone_store }}" id="recipient-name"
+                                                            readonly>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="message-text" class="col-form-label">Tanggal
+                                                            Diajukan:</label>
+                                                        <input type="text" class="form-control"
+                                                            value="{{ $s->updated_at }}" id="recipient-name" readonly>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn bg-gradient-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </tbody>
                     </table>
@@ -410,6 +432,7 @@
                     <div class="copyright text-center text-sm text-muted text-lg-start">
                         © <script>
                             document.write(new Date().getFullYear())
+
                         </script>,
                         made with <i class="fa fa-heart"></i> by
                         <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative
