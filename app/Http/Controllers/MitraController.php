d124ec12c4e9b48a1e09e76e2cdd9db8760386a0
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-06-24 16:15:52 +0700

    Fix Bug dan Test Lagi, Integrasi direction maps to bangekel view

--- app/Http/Controllers/MitraController.php
+++ app/Http/Controllers/MitraController.php
@@ -146,8 +146,6 @@ class MitraController extends Controller
 
         if (!$validatedData) {
             return redirect('store-register');
-        } else {
-            return redirect('list-pengajuan-store')->with('success_update', 'Store has been added');
         }
 
         $name = time() . "_" . $request->store_image->getClientOriginalName();
@@ -165,6 +163,6 @@ class MitraController extends Controller
         ]);
 
         $request->store_image->move(public_path('store_data/' . DB::getPdo()->lastInsertId() . '/image'), $name);
-        return redirect('store-register');
+        return redirect('list-pengajuan-store')->with('success_update', 'Store has been added');
     }
 }
